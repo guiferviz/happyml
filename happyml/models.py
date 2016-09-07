3bb5e48ad0916a38562387c9c1411c2ab636f3de
guiferviz <guiferviz@gmail.com>
2016-09-07 13:53:21 +0200

    Pocket perceptron algorithm.

--- happyml/models.py
+++ happyml/models.py
@@ -29,18 +29,43 @@ class Perceptron(Hypothesis):
         # FIXME: np.sign output is in {-1, 0, +1}
         return np.sign(np.dot(self.w.T, x) + self.b)
 
+    def _h(self, x):
+        return np.dot(self.w.T, x) + self.b
+
     def predict(self, X):
         return np.sign(np.dot(X, self.w) + self.b)
 
     def pla(self, X, y, iterations=10):
         """Perceptron Learning Algorithm."""
+        y = y.flatten()
         for i in range(iterations):
             for j in range(X.shape[0]):
                 x = X[j, :].T
-                out = self.h(x)
-                diff = (y[j] - out)
-                self.w += diff * x
-                self.b += diff
+                if self.h(x) != y[j]:
+                    self.w += y[j] * x
+                    self.b += y[j]
+
+    def pocket(self, X, y, iterations=10):
+        max_accuracy = 0.0
+        y = y.flatten()
+        pocket_w = np.array(self.w)
+        pocket_b = self.b
+        idx = np.arange(X.shape[0])
+        for iteration in range(iterations):
+            np.random.shuffle(idx)
+            for i in idx:
+                x = X[i, :].T
+                if self.h(x) != y[i]:
+                    self.w += y[i] * x
+                    self.b += y[i]
+                    accuracy = self.accuracy(X, y)
+                    if accuracy > max_accuracy:
+                        max_accuracy = accuracy
+                        pocket_w = np.array(self.w)
+                        pocket_b = self.b
+                    break
+        self.w = pocket_w
+        self.b = pocket_b
 
 
 class LinearRegression(Hypothesis):
